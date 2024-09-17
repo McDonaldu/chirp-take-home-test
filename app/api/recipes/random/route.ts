3c2c579758f01d7cb2588898a56c93eccdca935c
Michael <jluz@gbox.adnu.edu.ph>
2024-09-18 04:32:27 +0800

    [Modification] - Finalized cache issue fix

--- app/api/recipes/random/route.ts
+++ app/api/recipes/random/route.ts
@@ -7,21 +7,10 @@ export async function GET(request: NextRequest) {
     const { data } = await axios.get(
       "https://www.themealdb.com/api/json/v1/1/random.php"
     );
-    return NextResponse.json(
-      {
-        success: true,
-        data: data.meals[0],
-      },
-      {
-        headers: {
-          "Cache-Control":
-            "no-store, no-cache, must-revalidate, proxy-revalidate",
-          Pragma: "no-cache",
-          Expires: "0",
-          "Surrogate-Control": "no-store",
-        },
-      }
-    );
+    return NextResponse.json({
+      success: true,
+      data: data.meals[0],
+    });
   } catch (error) {
     console.log(error);
     if (error instanceof Error) {
